8b42ee6d8c139e8f327c29be2827487f133a80c5
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-06-09 17:23:27 -0600

    Added Legacy Forge entrypoint

---
+++ src/main/java/dev/neuralnexus/taterutils/platforms/LegacyForgePlugin.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca The project is Licensed under <a
+ * href="https://github.com/p0t4t0sandwich/TaterUtils/blob/dev/LICENSE">GPL-3</a> The API is
+ * Licensed under <a
+ * href="https://github.com/p0t4t0sandwich/TaterUtils/blob/dev/LICENSE-API">MIT</a>
+ */
+package dev.neuralnexus.taterutils.platforms;
+
+import cpw.mods.fml.common.Mod;
+
+import dev.neuralnexus.taterlib.logger.LoggerAdapter;
+import dev.neuralnexus.taterutils.TaterUtils;
+
+/** Legacy Forge entry point. */
+@Mod(
+        modid = TaterUtils.PROJECT_ID,
+        name = TaterUtils.PROJECT_NAME,
+        useMetadata = true,
+        acceptableRemoteVersions = "*",
+        bukkitPlugin = TaterUtils.PROJECT_NAME)
+public class LegacyForgePlugin {
+    public LegacyForgePlugin() {
+        TaterUtils.instance()
+                .pluginStart(this, null, null, new LoggerAdapter(TaterUtils.PROJECT_ID));
+    }
+}
